a935ec94a8efd9b090732ba58aef1d9ab6092e7b
vishalkrsharma <vishalkrsharma1234@gmail.com>
2021-09-21 15:45:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Alert from './components/Alert';
+import Form from './components/Form';
+// import About from './components/About.js';
+
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (type, message) => {
+    setAlert({
+      type: type,
+      msg: message,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      showAlert('success', 'DarkMode');
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+      showAlert('success', 'LightMode');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Router> */}
+        <Navbar hero='react' mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        {/* <Switch> */}
+          {/* <Route exact path='/about'>
+            <About />
+          </Route> */}
+          {/* <Route exact path='/'> */}
+            <Form title='Enter' showAlert={showAlert} />
+          {/* </Route> */}
+        {/* </Switch> */}
+      {/* </Router> */}
+    </>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <div className="container">
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim a voluptates nobis ipsa asperiores laudantium quaerat sunt exercitationem illum quia, ea tempora accusamus? Ad itaque perspiciatis exercitationem in debitis tempora quos aspernatur quisquam sint ea culpa iusto, vel quae ullam! Delectus ipsa ipsum eum consequuntur recusandae vel eveniet rem fuga ratione neque dolorum ad, animi deserunt quisquam accusantium voluptatem odio inventore possimus fugiat accusamus! Provident, inventore quis cupiditate sunt dolores distinctio nesciunt ratione reprehenderit itaque quia soluta placeat minima beatae deleniti eligendi nam nihil, assumenda officiis culpa aliquid, vero fugit harum. Earum enim dignissimos esse consectetur ipsam molestiae aliquam et.</h3>
+      </div>
+    </>
+  );
+}
+
+export default About;
